8767b192ae267e8d3e67aef8c8aec7506e8da6a1
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2024-08-02 13:37:25 +0800

    Update Waku.sh

--- Waku.sh
+++ Waku.sh
@@ -66,8 +66,9 @@ function install_docker() {
     # 检查是否安装了 docker-compose，如果未安装则安装
     if ! command -v docker-compose &> /dev/null; then
         echo "docker-compose 未安装，正在安装 docker-compose..."
-        sudo curl -L "https://github.com/docker/compose/releases/download/v2.20.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+        sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
         sudo chmod +x /usr/local/bin/docker-compose
+        docker-compose --version  # 版本应为 docker-compose version 1.29.2
         if command -v docker-compose &> /dev/null; then
             echo "docker-compose 安装成功。"
         else
